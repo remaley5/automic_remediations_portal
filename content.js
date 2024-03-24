22690e364e5998e68237ad77e0aeddb16fc7ee18
Sophie Willson <sophierwillson@gmail.com>
2024-03-23 18:49:02 -0700

    added input field for automic title, scroll and  focus on submit, focus checkbox

--- content.js
+++ content.js
@@ -1,23 +1,50 @@
+
+const sendFocus = function () {
+    document.querySelector('button[data-cy="createManualRemButton"]').addEventListener("click", function () {
+
+    });
+}
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    //console.log("in event listener, request.action", request.action);
+
+    let openManualRem = function () {
+        setTimeout(function () {
+            const manualRemsLength = document.querySelectorAll('table[data-cy="manualRemTable"] tr').length;
+            
+            
+            console.log('before before focus', document.querySelectorAll('table[data-cy="manualRemTable"] tr')[manualRemsLength]);
+            document.querySelector('[data-cy="createManualRemModal"] input#field-name')
+                .value = request.title;
+            document.querySelector('[data-cy="createManualRemModal"] input#field-name').focus();
+            console.log('ae send focus?', request.send_focus);
+            
+            
+            // If focus is checked then send focus to the new input when manual remediation is saved
+            if (request.send_focus === true) {
+                document.querySelector('[data-cy="createManualRemModal"] button[data-cy="submitButton"]').addEventListener("click", function () {
+                    console.log('before focus', document.querySelectorAll('table[data-cy="manualRemTable"] tr')[manualRemsLength]);
+                    
+                    setTimeout(function () {
+                        let newRem = document.querySelectorAll('table[data-cy="manualRemTable"] tr')[manualRemsLength];
+                        newRem.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+                        newRem.setAttribute('tabindex', '0');
+                        newRem.focus();
+                    }, 1200);
+                });
+            }
+        }, 500);
+    }
+
     if (request.action === "set") {
         // click event for adding click event
-        console.log("setting click event");
+        console.log('send focus?', request.send_focus);
         document
             .querySelector('button[data-cy="createManualRemButton"]')
-            .setAttribute("testing", "yes");
-        document
-            .querySelector('button[data-cy="createManualRemButton"]')
-            .addEventListener("click", function () {
-                console.log("clicked manual rem button");
-                // setTimeout(function () {
-
-                // }, 500);
-            });
-
+            .addEventListener("click", openManualRem);
         sendResponse({ message: "That's setting something" });
     } else if (request.action === "reset") {
-        // resetting something
+        document
+            .querySelector('button[data-cy="createManualRemButton"]')
+            .removeEventListener("click", openManualRem);
         sendResponse({ message: "That's resetting something" });
     }
 });
--- popup.js
+++ popup.js
@@ -1,13 +1,19 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    var setButton = document.getElementById("setButton");
-    var resetButton = document.getElementById("resetButton");
+    
+    // Elements
+    const setButton = document.getElementById("set_button");
+    const resetButton = document.getElementById("reset_button");
+    const title = document.getElementById("automic_name");
+    const send_focus = document.getElementById("send_focus");
 
     setButton.addEventListener("click", function () {
-        console.log("clicked set button");
+        // console.log("clicked set button", title.value);
         chrome.tabs.query(
             { active: true, currentWindow: true },
             function (tabs) {
-                chrome.tabs.sendMessage(tabs[0].id, { action: "set" });
+                chrome.tabs.sendMessage(tabs[0].id, { action: "set", title: title.value, send_focus: send_focus.checked });
             }
         );
     });
@@ -15,7 +21,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // testing 
     resetButton.addEventListener("click", function () {
-        console.log("clicked reset button");
+        // console.log("clicked reset button");
+        title.value = '';
+        send_focus.checked = false;
         chrome.tabs.query(
             { active: true, currentWindow: true },
             function (tabs) {
